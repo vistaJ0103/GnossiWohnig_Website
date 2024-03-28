20d043aa368f98742d33e45f7b341e50d35df177
TecFoxGmbH <85845378+TecFoxGmbH@users.noreply.github.com>
2024-03-28 22:32:14 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDoPyV11X1-HBgUy9ZxjX5oT3ljWutHD-o",
+  apiKey: "",
   authDomain: "gnossiwohnig.firebaseapp.com",
   projectId: "gnossiwohnig",
   storageBucket: "gnossiwohnig.appspot.com",
-  messagingSenderId: "785702256902",
-  appId: "1:785702256902:web:aa9cab56fa0c8a9b915e87",
-  measurementId: "G-T1P295Y65Q",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 export default firebaseConfig;
