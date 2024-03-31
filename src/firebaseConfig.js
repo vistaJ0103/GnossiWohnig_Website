5a827def8f101bab08ab72b4d60b0289b1bfb40e
Cornelia Scherrer <corneliascherrer@Cornelias-MacBook-Pro.local>
2024-03-31 09:51:31 +0200

    config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: "AIzaSyDoPyV11X1-HBgUy9ZxjX5oT3ljWutHD-o",
   authDomain: "gnossiwohnig.firebaseapp.com",
   projectId: "gnossiwohnig",
   storageBucket: "gnossiwohnig.appspot.com",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  messagingSenderId: "785702256902",
+  appId: "1:785702256902:web:aa9cab56fa0c8a9b915e87",
+  measurementId: "G-T1P295Y65Q",
 };
 
 export default firebaseConfig;
